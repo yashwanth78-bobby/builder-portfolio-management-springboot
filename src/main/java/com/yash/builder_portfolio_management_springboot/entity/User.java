094da94b4418d0e4d6cebc615adc8522caf6f477
yashwanth <yashwanth.mamilla07@gmail.com>
2025-07-27 10:36:35 +0530

    added user entity and Role enum

---
+++ src/main/java/com/yash/builder_portfolio_management_springboot/entity/User.java
@@ -0,0 +1,22 @@
+package com.yash.builder_portfolio_management_springboot.entity;
+
+
+import com.yash.builder_portfolio_management_springboot.enums.Role;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    private String userEmailId;
+    private String passwordHash;
+    private Role role;
+}
---
+++ src/main/java/com/yash/builder_portfolio_management_springboot/enums/Role.java
@@ -0,0 +1,17 @@
+package com.yash.builder_portfolio_management_springboot.enums;
+
+public enum Role {
+    ADMIN,
+    BUILDER,
+    CLIENT,
+    PROJECT_MANAGER;
+
+    public static Role fromString(String roleStr) {
+        for (Role role : Role.values()) {
+            if (role.name().equalsIgnoreCase(roleStr)) {
+                return role;
+            }
+        }
+        throw new IllegalArgumentException("Invalid role: " + roleStr);
+    }
+}
