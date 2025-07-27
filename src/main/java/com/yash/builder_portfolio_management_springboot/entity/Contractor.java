7314a53a44a1444760c1cf4362deefd5b9f197e0
yashwanth <yashwanth.mamilla07@gmail.com>
2025-07-27 10:42:55 +0530

    added contractor entity

---
+++ src/main/java/com/yash/builder_portfolio_management_springboot/entity/Contractor.java
@@ -0,0 +1,24 @@
+package com.yash.builder_portfolio_management_springboot.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+class Contractor {
+    @Id
+    private String builderEmailId;
+    private String builderName;
+    private String builderPhoneNumber;
+    private int addressId;
+    private int registrationNumber;
+
+}
