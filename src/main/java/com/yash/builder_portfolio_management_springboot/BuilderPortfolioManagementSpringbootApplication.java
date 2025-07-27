bb035a89d8f3b79b356857dbff2f008098765e18
yashwanth <yashwanth.mamilla07@gmail.com>
2025-07-27 10:27:44 +0530

    added db configuration

---
+++ src/main/java/com/yash/builder_portfolio_management_springboot/BuilderPortfolioManagementSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.yash.builder_portfolio_management_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuilderPortfolioManagementSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuilderPortfolioManagementSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yash/builder_portfolio_management_springboot/BuilderPortfolioManagementSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash.builder_portfolio_management_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuilderPortfolioManagementSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
